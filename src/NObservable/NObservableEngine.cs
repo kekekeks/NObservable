de6cb7eb3443ed0d79f984745c2162056f4d8455
Nikita Tsukanov <keks9n@gmail.com>
2018-08-31 11:36:22 +0300

    Implemented When

--- src/NObservable/NObservableEngine.cs
+++ src/NObservable/NObservableEngine.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using NObservable.Scheduling;
 
 namespace NObservable
@@ -44,5 +45,67 @@ namespace NObservable
         {
             Context.RunInAction(action);
         }
+
+        public static Task When(Func<bool> condition) => When(condition, (AutorunOptions) null);
+        public static Task When(Func<bool> condition, AutorunOptions options)
+        {
+            var tcs = new TaskCompletionSource<int>();
+            IDisposable autorun = null;
+            autorun = Autorun(() =>
+            {
+                try
+                {
+                    if (condition())
+                    {
+                        autorun?.Dispose();
+                        tcs.TrySetResult(0);
+                    }
+                }
+                catch (Exception e)
+                {
+                    autorun?.Dispose();
+                    tcs.TrySetException(e);
+                }
+
+            }, options);
+            return tcs.Task;
+        }
+
+        public static IDisposable When(Func<bool> condition, Action effect) =>
+            When(condition, effect, null);
+        
+        public static IDisposable When(Func<bool> condition, Action effect, AutorunOptions options)
+        {
+            IDisposable autorun = null;
+            autorun = Autorun(() =>
+            {
+                var runEffect = false;
+                var dispose = true;
+                try
+                {
+                    if (condition())
+                    {
+                        runEffect = true;
+                    }
+                    else
+                    {
+                        // Avoid having a catch block in order to not disrupt debugging 
+                        // This indicates that exception has NOT happened and condition()==FALSE
+                        dispose = false;
+                    }
+                }
+                finally
+                {
+                    if (dispose)
+                        autorun?.Dispose();
+                }
+
+                if (runEffect)
+                    effect();
+            }, options);
+            return autorun;
+        }
+        
+        
     }
 }
---
+++ tests/ObservableTests/WhenTests.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Threading.Tasks;
+using NObservable;
+using Xunit;
+
+namespace ObservableTests
+{
+    public class WhenTests : TestBase
+    {
+        [Observable]
+        class Observable
+        {
+            public int Value { get; set; }
+        }
+
+        [Fact]
+        public void When_Effect_Should_Trigger_Once_When_Condition_Is_Reached()
+        {
+            var ob = new Observable();
+            int triggerNumber = 0;
+            NObservableEngine.When(() => ob.Value == 10, () => { triggerNumber++; });
+            ob.Value = 5;
+            ob.Value = 6;
+            Assert.Equal(0, triggerNumber);
+            ob.Value = 10;
+            Assert.Equal(1, triggerNumber);
+            ob.Value = 5;
+            ob.Value = 10;
+            Assert.Equal(1, triggerNumber);
+        }
+        
+        [Fact]
+        public void When_Should_Be_Disposable()
+        {
+            var ob = new Observable();
+            int triggerNumber = 0;
+            var d = NObservableEngine.When(() => ob.Value == 10, () => { triggerNumber++; });
+            ob.Value = 5;
+            ob.Value = 6;
+            Assert.Equal(0, triggerNumber);
+            d.Dispose();
+            ob.Value = 10;
+            Assert.Equal(0, triggerNumber);
+            ob.Value = 5;
+            ob.Value = 10;
+            Assert.Equal(0, triggerNumber);
+        }
+
+        [Fact]
+        public void When_Task_Should_Trigger_When_Condition_Is_Reached()
+        {
+            var ob = new Observable();
+            var triggered = false;
+            NObservableEngine.When(() => ob.Value == 10)
+                .ContinueWith(t =>
+                {
+                    if (t.IsCompletedSuccessfully)
+                        triggered = true;
+                }, TaskContinuationOptions.ExecuteSynchronously);
+            ob.Value = 5;
+            Assert.False(triggered);
+            ob.Value = 10;
+            Assert.True(triggered);
+        }
+        
+        [Fact]
+        public void When_Task_Should_Propagate_Condition_Exceptions()
+        {
+            var ob = new Observable();
+            var triggered = false;
+            var exception = false;
+            NObservableEngine.When(() =>
+                {
+                    if(ob.Value == 10)
+                        throw new Exception("TEST");
+                    return false;
+                })
+                .ContinueWith(t =>
+                {
+                    if (t.IsCompletedSuccessfully)
+                        triggered = true;
+                    if (t.IsFaulted && t.Exception.InnerException.Message == "TEST")
+                        exception = true;
+                }, TaskContinuationOptions.ExecuteSynchronously);
+            ob.Value = 5;
+            Assert.False(triggered);
+            Assert.False(exception);
+            ob.Value = 10;
+            Assert.False(triggered);
+            Assert.True(exception);
+            
+        }
+        
+    }
+}
